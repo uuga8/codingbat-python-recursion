0a587c239d996f177bc79051339e5de461f0b736
uuga8 <76564742+uuga8@users.noreply.github.com>
2021-01-05 21:41:50 +0200

    Update R00_factorial.py

--- recursion1/R00_factorial.py
+++ recursion1/R00_factorial.py
@@ -2,16 +2,11 @@ from unittest import TestCase
 
 
 def factorial(n: int) -> int:
-    """
-    Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1. Compute the result recursively
-    (without loops).
-    
-    Examples:
-    factorial(1) → 1
-    factorial(2) → 2
-    factorial(3) → 6
-    """
-    pass
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+print(factorial(6))
 
 
 class Test(TestCase):
