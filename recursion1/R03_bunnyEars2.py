2922f762c1b6599ae476f027cb766a9c6e993481
uuga8 <ilga.mozule@Inbox.lv>
2021-01-06 22:20:05 +0200

    Shorten the return statement with a one-liner for R03_bunnyEars2.py

--- recursion1/R03_bunnyEars2.py
+++ recursion1/R03_bunnyEars2.py
@@ -14,11 +14,7 @@ def bunnyEars2(bunnies: int) -> int:
     """
     if bunnies == 0:
         return 0
-    else:
-        if bunnies % 2 == 0:
-            return 3 + bunnyEars2(bunnies-1)
-        else:
-            return 2 + bunnyEars2(bunnies-1)
+    return (3 if bunnies % 2 == 0 else 2) + bunnyEars2(bunnies -1)
 
 class Test(TestCase):
     def test1(self):
